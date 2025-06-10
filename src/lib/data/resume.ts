139bbc14296990d7a5bcd7362f6dd4b39094131f
Harsha-Udutha <harshaudutha.us@gmail.com>
2025-06-10 12:31:19 -0500

    updated GD resume Link

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,3 @@
-export const data = 'https://drive.google.com/file/d/1qM76zm9Y1bXkNQN1GLuwdiCqnB1UBAkt/view?usp=sharing';
+export const data = 'https://drive.google.com/file/d/1Auy5jyddgTGJJxneCt_1xH0JTr13eShV/view?usp=sharing';
 
 export const title = 'Resumé';
