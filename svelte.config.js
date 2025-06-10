33f20c924676916c732f0841070c9da372755922
Harsha-Udutha <harshaudutha.us@gmail.com>
2025-06-09 21:29:49 -0500

    changing the config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const isProd = process.env.NODE_ENV === 'production';
+const base = '/slick-portfolio-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	vitePlugin: {
 		inspector: {
@@ -22,7 +24,7 @@ const config = {
 			'@utils': './src/lib/utils'
 		},
 		paths: {
-			base: isProd ? '/slick-portfolio-svelte' : ''
+			base: process.env.NODE_ENV === 'production' ? base : 'portfolio'
 		}
 	}
 };
