393f08ebe747eaa8b83407762ee0a04c0aa0fe36
Harsha-Udutha <harshaudutha.us@gmail.com>
2025-10-04 00:03:31 -0400

    updated Resume

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,3 @@
-export const data = 'https://drive.google.com/file/d/1-VrFKklcZ9bMhU4PCVa76HnzAllFJIwy/view?usp=sharing';
+export const data = 'https://drive.google.com/file/d/1jCwZ4uoecNRUo7UIldgDMONjoPu-MJ04/view?usp=sharing';
 
 export const title = 'RESUME';
