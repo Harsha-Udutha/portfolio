4b1784ab358907d992ea5d7db11c880d915793ed
Harsha-Udutha <harshaudutha.us@gmail.com>
2025-12-09 18:53:23 -0500

    updated resume with google drive link

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,3 @@
-export const data = 'https://drive.google.com/file/d/1jCwZ4uoecNRUo7UIldgDMONjoPu-MJ04/view?usp=sharing';
+export const data = 'https://drive.google.com/file/d/1IIVnHnN38cs3d-HsEbcDQqefagirSIEp/view?usp=sharing';
 
 export const title = 'RESUME';
